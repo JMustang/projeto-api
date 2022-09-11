5f1f362a4c7b1fa8031480e34dbaaea981742abb
Junior <eecfredes@gmail.com>
2022-09-11 17:29:43 -0300

    :books:docs: SWAGGER add

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Projeto API')
+    .setDescription('A api made javascript using the Nestjs framework')
+    .setVersion('1.0.0')
+    .addTag('api')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+  await app.listen(3000);
+}
+bootstrap();
