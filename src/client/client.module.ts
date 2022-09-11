67dc32db3897fe93d04ffd09a6e31c3541e9e7ca
Junior <eecfredes@gmail.com>
2022-09-11 19:50:13 -0300

    :sparkles:feat: Client module config seting up

--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { ClientController } from './client.controller';
+import { DatabaseModule } from 'src/database/database.module';
+import { clienteProviders } from './client.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ClientController],
-  providers: [ClientService]
+  providers: [...clienteProviders, ClientService],
 })
 export class ClientModule {}
